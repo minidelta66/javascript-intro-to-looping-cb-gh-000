5a516fa6127cce80c7167788d322abaf30ac44a1
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-25 16:21:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 function forLoop(array) {
   for (i = 1; i <25; i++) {
       if (i === 1) {
-        return array.push("I am 1 strange loop.");
+    array.push("I am 1 strange loop.");
       }
     else{
-    return array.push("I am " + i + " strane loop" + (i==0 ? '' : 's') + '.');
+    array.push("I am " + i + " strane loop" + (i==0 ? '' : 's') + '.');
   }
 }
+return array;
 }
